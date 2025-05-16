3cadcddef3aa0749915a55976a020e3d66b9d526
Miachi Solomon <smiachi10@gmail.com>
2025-05-16 12:31:48 +0100

    adding the ui and working on functionality

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let display = document.querySelector('p').textContent;
+let numbers = document.querySelectorAll('.numbers');
+
 function add(a, b) {
     return a + b;
 }
@@ -17,3 +20,15 @@ function divide(a, b) {
 let first;
 let second;
 let operator;
+
+
+const operate = (operator, a, b) => operator(a, b);
+
+Array.from(numbers).map(number => number.addEventListener('click',(value) => {
+    if (document.querySelector('p').textContent == 0){
+        document.querySelector('p').textContent = "";
+    }
+    document.querySelector('p').textContent += value.target.textContent;
+}));
+
+// console.log(operate(add, 2, 3))
