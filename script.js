231564d7f5d742c053666c933b4620d8facf450e
Miachi Solomon <smiachi10@gmail.com>
2025-05-17 23:15:07 +0100

    creating the MVP of the calculator with necessary features

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-let display = document.querySelector('p').textContent;
+let display = document.querySelector('p');
 let numbers = document.querySelectorAll('.numbers');
+let equalSign = document.querySelector('.equal-to');
+let operatorSigns = document.querySelectorAll('.operator');
+
 
 function add(a, b) {
     return a + b;
@@ -20,15 +23,43 @@ function divide(a, b) {
 let first;
 let second;
 let operator;
+let sign;
 
-
-const operate = (operator, a, b) => operator(a, b);
+function operate(operator, a, b) {
+    return operator(a, b);
+}
 
 Array.from(numbers).map(number => number.addEventListener('click',(value) => {
-    if (document.querySelector('p').textContent == 0){
-        document.querySelector('p').textContent = "";
-    }
+    if (display.textContent == 0){
+        display.textContent = "";
+    } 
     document.querySelector('p').textContent += value.target.textContent;
 }));
 
-// console.log(operate(add, 2, 3))
+Array.from(operatorSigns).map(operatorSign => operatorSign.addEventListener('click', (value) => {
+        sign = operatorSign.textContent;
+        operator = operatorSign.textContent;
+        display.textContent += value.target.textContent;
+        
+        switch (operator) {
+            case '+':
+                operator = add;
+                break;
+            case '-':
+                operator = subtract;
+                break;
+            case 'X':
+                operator = multiply;
+                break;
+            case '/':
+                operator = divide;
+                break;
+        }
+}));
+
+equalSign.addEventListener('click', () => {
+    let signIndex = display.textContent.indexOf(sign);
+    first = Number(display.textContent.slice(0, signIndex));
+    second = Number(display.textContent.slice(++signIndex));
+    display.textContent = operate(operator, first, second);
+});
