b76306177925e1ec9b755ace7aad4dffd3910942
Miachi Solomon <smiachi10@gmail.com>
2025-05-27 22:03:34 +0100

    code refactoring and ux improvement

--- script.js
+++ script.js
@@ -39,10 +39,10 @@ function operate(operator, a, b) {
 let first;
 let second;
 let operator;
-let numberWasClicked = false;
+let numberWasClicked;
+let operatorWasClicked;
 
 function clickNumber(value) {
-    Array.from(operatorSigns).map(operatorSign => operatorSign.classList.remove('operator-btn-right-hand-clicked'));
     element.remove();
     display.textContent += value.target.textContent;
     numberWasClicked = true;
@@ -52,17 +52,21 @@ Array.from(numbers).map(number => number.addEventListener('click', clickNumber))
 
 
 Array.from(operatorSigns).map(operatorSign => operatorSign.addEventListener('click', () => {
+    operatorWasClicked = true;
     operatorSign.classList.add('operator-btn-right-hand-clicked');
     operator = operatorSign.textContent;
-    first = display.textContent.slice(0);
-    console.log(first);
-    numberWasClicked = false;
+    first = Number(display.textContent.slice(0));
 
-    if(!numberWasClicked) {
-        clickNumber();
+    if (operatorWasClicked) {
+        Array.from(numbers).map(number => number.addEventListener('click', (value) => {
+            if (operatorSign.classList.contains('operator-btn-right-hand-clicked')) {
+                operatorSign.classList.remove('operator-btn-right-hand-clicked');
+                display.textContent = value.target.textContent;
+            }
+            second = Number(display.textContent.slice(0));
+        
+        }));
     }
-
-
     switch (operator) {
         case '+':
             operator = add;
@@ -80,13 +84,14 @@ Array.from(operatorSigns).map(operatorSign => operatorSign.addEventListener('cli
 }));
 
 equalSign.addEventListener('click', () => {
-
+    display.textContent = operate(operator, first, second);
 });
 
 clearBtn.addEventListener('click', () => {
     display.textContent = '';
     element.textContent = '0';
     myDiv.appendChild(element);
+    Array.from(operatorSigns).map(operatorSign => operatorSign.classList.remove('operator-btn-right-hand-clicked'));
 });
 
 percentageBtn.addEventListener('click', () => {
